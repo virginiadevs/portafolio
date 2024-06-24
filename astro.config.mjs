59c76ec29e0f13d54aa3c4c816d9cda602d060a0
Virginia Calvo <57789020+virginiadevs@users.noreply.github.com>
2024-06-24 13:42:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://virginiadevs.github.io/portafolio'
+    site: 'https://virginiadevs.github.io/portafolio',
+    base: '/portafolio'
 });
